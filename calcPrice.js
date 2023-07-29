273e227a26da6fda0d2936f2b3e1ee1762322e68
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-07-29 02:10:55 -0300

    adding cellphone functionality to the selector and price calculator

--- calcPrice.js
+++ calcPrice.js
@@ -1,13 +1,9 @@
-
 function calculoCosto(){
   const precioHarina = document.querySelector("#precioHarina").value;
   const precioLevadura = document.querySelector("#precioLevadura").value;
   let costoPorPan = (precioHarina*.5) + (precioLevadura*.2);
-
   const resDisplay = document.querySelector(".price-res");
   resDisplay.innerHTML = `Cada pan tiene un costo de $${costoPorPan}`;
-  
-
 };
-
-document.querySelector("#precioButton").addEventListener("click", calculoCosto)
+document.querySelector("#precioButton").addEventListener("click", calculoCosto);
+document.querySelector("#precioButton").addEventListener("touchend", calculoCosto)
--- calcSelector.js
+++ calcSelector.js
@@ -3,20 +3,23 @@ const articuloCantidad = document.querySelector("#cantidad");
 const selectorPrecio =  document.querySelector("#selector-precio");
 const selectorCantidad = document.querySelector("#selector-cantidad");
 
-
-document.querySelector("#selector-precio").addEventListener("click", ()=>{
+const changeToPrice = ()=>{
   articuloPrecio.classList.remove("hide");
   articuloCantidad.classList.add("hide");
   selectorPrecio.classList.add("seccion-activa");
   selectorPrecio.classList.remove("seccion");
   selectorCantidad.classList.remove("seccion-activa");
   selectorCantidad.classList.add("seccion");
-})
-document.querySelector("#selector-cantidad").addEventListener("click", ()=>{
+}
+const changeToCant =  ()=>{
   articuloPrecio.classList.add("hide");
   articuloCantidad.classList.remove("hide");
   selectorPrecio.classList.remove("seccion-activa");
   selectorPrecio.classList.add("seccion");
   selectorCantidad.classList.add("seccion-activa");
   selectorCantidad.classList.remove("seccion");
-})
+}
+document.querySelector("#selector-precio").addEventListener("click", changeToPrice);
+document.querySelector("#selector-precio").addEventListener("touchend", changeToPrice);
+document.querySelector("#selector-cantidad").addEventListener("click", changeToCant);
+document.querySelector("#selector-cantidad").addEventListener("touchend", changeToCant);
