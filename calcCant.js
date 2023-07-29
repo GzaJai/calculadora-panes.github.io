7e7956d3c9b8645e53ee164d1ef7ee0ffee28381
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-07-29 00:56:41 -0300

    adding the logic to the quantity calculator

--- calcCant.js
+++ calcCant.js
@@ -2,16 +2,13 @@
 
 function harinaCalc (){
   const inputCant = document.querySelector("#cantInput").value;
-
   if((inputCant*.5) >= 1){
     var hRes = "Se necesitan " +(inputCant*.5) +"Kg de harina.";
   }else if(inputCant < 1){
     var hRes = "ERROR";
   }else{ 
     var hRes = "Se necesitan " +(inputCant*.5)*1000+"g de harina."};
-
   let cantHarina = hRes;
-
   const restList = document.querySelector("#res-list");
   const oldChild = document.querySelector("#h-list-item");
   if(oldChild == undefined){  
@@ -22,11 +19,8 @@ function harinaCalc (){
     var hli = document.createElement("li");
     hli.setAttribute("id","h-list-item");
   }
-
   hli.innerText = `${cantHarina}`;
-
   restList.append(hli);
-
   console.log("ok");
 }
 
@@ -35,7 +29,6 @@ function harinaCalc (){
 function levaduraCalc (){
   const inputCant = document.querySelector("#cantInput").value;
   const lConvert = (inputCant*.01);
-
   if((lConvert) >= 1){
     var lRes = "Se necesitan " + lConvert +"Kg de levadura.";
   }else if(inputCant < 1){
@@ -44,7 +37,6 @@ function levaduraCalc (){
     var lRes = "Se necesitan " + lConvert*1000+"g de levadura."};
 
   let cantLevadura = lRes;
-
   const restList = document.querySelector("#res-list");
   const oldChild = document.querySelector("#l-list-item");
   if(oldChild == undefined){  
@@ -55,20 +47,14 @@ function levaduraCalc (){
     var lli = document.createElement("li");
     lli.setAttribute("id","l-list-item");
   }
-
-
   lli.innerText = `${cantLevadura}`;
-
   restList.append(lli);
-
   console.log("ok");
 }
 
 function showRes(){
   harinaCalc();
   levaduraCalc();
-  
   document.querySelector(".init-mes").classList.add("hide");
 }
-
 document.querySelector("#cantButton").addEventListener("click", showRes);
