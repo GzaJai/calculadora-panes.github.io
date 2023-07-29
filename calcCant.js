a2644fc25b440bc7842451914f37b4c5433862b1
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-07-29 00:54:37 -0300

    adding the logic to the quantity calculator

---
+++ calcCant.js
@@ -0,0 +1,74 @@
+//  calculos de la harina necesaria
+
+function harinaCalc (){
+  const inputCant = document.querySelector("#cantInput").value;
+
+  if((inputCant*.5) >= 1){
+    var hRes = "Se necesitan " +(inputCant*.5) +"Kg de harina.";
+  }else if(inputCant < 1){
+    var hRes = "ERROR";
+  }else{ 
+    var hRes = "Se necesitan " +(inputCant*.5)*1000+"g de harina."};
+
+  let cantHarina = hRes;
+
+  const restList = document.querySelector("#res-list");
+  const oldChild = document.querySelector("#h-list-item");
+  if(oldChild == undefined){  
+    var hli = document.createElement("li");
+    hli.setAttribute("id","h-list-item");
+  }else{
+    restList.removeChild(oldChild);
+    var hli = document.createElement("li");
+    hli.setAttribute("id","h-list-item");
+  }
+
+  hli.innerText = `${cantHarina}`;
+
+  restList.append(hli);
+
+  console.log("ok");
+}
+
+// calculos de la levadura necesaria
+
+function levaduraCalc (){
+  const inputCant = document.querySelector("#cantInput").value;
+  const lConvert = (inputCant*.01);
+
+  if((lConvert) >= 1){
+    var lRes = "Se necesitan " + lConvert +"Kg de levadura.";
+  }else if(inputCant < 1){
+    var lRes = "Ingrese un número válido.";
+  }else{ 
+    var lRes = "Se necesitan " + lConvert*1000+"g de levadura."};
+
+  let cantLevadura = lRes;
+
+  const restList = document.querySelector("#res-list");
+  const oldChild = document.querySelector("#l-list-item");
+  if(oldChild == undefined){  
+    var lli = document.createElement("li");
+    lli.setAttribute("id","l-list-item");
+  }else{
+    restList.removeChild(oldChild);
+    var lli = document.createElement("li");
+    lli.setAttribute("id","l-list-item");
+  }
+
+
+  lli.innerText = `${cantLevadura}`;
+
+  restList.append(lli);
+
+  console.log("ok");
+}
+
+function showRes(){
+  harinaCalc();
+  levaduraCalc();
+  
+  document.querySelector(".init-mes").classList.add("hide");
+}
+
+document.querySelector("#cantButton").addEventListener("click", showRes);
